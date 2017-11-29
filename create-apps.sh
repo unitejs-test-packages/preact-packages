0934b07c081ed1c2c499f3c3d3a18b66b1d18384
Martyn Janes <martyn@obany.com>
2017-11-29 08:41:57 +0300

    Initial Commit

--- create-apps.sh
+++ create-apps.sh
@@ -9,4 +9,3 @@ node unitejs/cli/bin/unite configure --packageName=preact-typescript-amd --title
 
 node unitejs/cli/bin/unite configure --packageName=preact-typescript-systemjs --title="Preact TypeScript SystemJS" --profile=PreactTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --unitTestRunner=Karma --unitTestEngine=ChromeHeadless --outputDirectory=./apps/preact-typescript-systemjs
 # node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/preact-typescript-systemjs
-
