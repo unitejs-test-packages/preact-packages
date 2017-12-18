e7a785aa6c1b7479b063da4cc0ceefc6ae963e4e
Martyn Janes <martyn@obany.com>
2017-12-18 10:27:39 +0300

    Added JavaScript SystemJS Builds

--- create-apps.sh
+++ create-apps.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 
-node unitejs/unitejs-cli/bin/unite configure --packageName=preact-javascript --title="Preact JavaScript" --profile=PreactJavaScript --outputDirectory=./apps/preact-javascript
-mkdir -p ./apps/preact-javascript/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
-mkdir -p ./apps/preact-javascript/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
-node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/preact-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/preact-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/preact-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/preact-javascript
+node unitejs/unitejs-cli/bin/unite configure --packageName=preact-javascript-commonjs --title="Preact JavaScript CommonJS" --profile=PreactJavaScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/preact-javascript-commonjs
+mkdir -p ./apps/preact-javascript-commonjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/preact-javascript-commonjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/preact-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/preact-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/preact-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/preact-javascript-commonjs
+
+node unitejs/unitejs-cli/bin/unite configure --packageName=preact-javascript-systemjs --title="Preact JavaScript SYstemJS" --profile=PreactJavaScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/preact-javascript-systemjs
+mkdir -p ./apps/preact-javascript-systemjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/preact-javascript-systemjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/preact-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/preact-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/preact-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/preact-javascript-systemjs
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=preact-typescript-commonjs --title="Preact TypeScript CommonJS" --profile=PreactTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/preact-typescript-commonjs
 mkdir -p ./apps/preact-typescript-commonjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.ts $_
